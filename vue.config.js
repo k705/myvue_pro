33d42fe07383bd9cecea9dd38445bcd047db56ad
study73 <464043271@qq.com>
2023-06-19 15:38:25 +0800

    vue.config中的服务器配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  //默认情况下 babel-loader 会忽略所有 node_modules 中的文件。你可以启用本选项，以避免构建后的代码中出现未转译的第三方依赖。
+  //不过，对所有的依赖都进行转译可能会降低构建速度
+  transpileDependencies: false,
+  // 服务器配置
+  devServer: {
+    // 是否自动打开浏览器
+    open: true,
+    // 主机地址(1.localhost  2.127.0.0.1 3.本地ip：cmd中查找ipconfig)
+    host: '192.168.16.71',
+    // 端口号 (0----65535,1-1023系统端口号,1024-5000属于应用端口)
+    port:8080
+  }
 })
