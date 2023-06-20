587d0abd707317b71d91f39509ea060d5c1181cc
study73 <464043271@qq.com>
2023-06-20 15:09:25 +0800

    vue.config配置

--- README.md
+++ README.md
@@ -22,3 +22,15 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+# 项目
+## vue.config配置
+1. 是否转译依赖transpileDependencies
+2. 服务器配置devServer
+    - open：是否自动打开浏览器
+    - host：本地ip地址
+    - port：端口号
+3. 打包配置configureWebpack
+    - 路径别名配置alias`"@": path.resolve(__dirname, "./src")`
+    - jsconfig中额外再配置就会有代码书写时的提示
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: false,
+  // 服务器配置
+  devServer: {
+    open: true,
+    host: "172.20.10.4",
+    port:8080
+  },
+  configureWebpack: {
+    resolve: {
+      //路径别名配置(需要在jsconfig中额外再配置就会有代码书写时的提示)
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@comp": "@/components",
+        "@api": "@/api",
+      },
+    },
+  },
 })
