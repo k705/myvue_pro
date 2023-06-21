5861f4bd1e838c046ae17eff7ebb2115c77cdf5e
study73 <464043271@qq.com>
2023-06-21 12:39:42 +0800

    路由组件搭建和主页静态搭建

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,16 @@ import routes from '@/router/routes'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
+    mode:"history",
     routes,
 })
-
+// 重写push
+const oldReplace = VueRouter.prototype.replace;
+VueRouter.prototype.replace = function (
+  loaction,
+  onComplate = () => {},
+  onAbort = () => {}
+) {
+  oldReplace.call(this, loaction, onComplate, onAbort);
+};
 export default router
