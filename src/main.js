6ec07d132dd6c63161bb52837a3f343d6c95fd4b
study73 <464043271@qq.com>
2023-06-27 10:48:47 +0800

    切换TypeNav为公共组件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "@/router"
+import TypeNav from "@/components/TypeNav"
 
 Vue.component("TypeNav",TypeNav)
 Vue.config.productionTip = false
